64a4166c563fbb686068e6bc0d51527a2590a28d
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-03-19 01:23:19 -0500

    Add files via upload

---
+++ datecc.py
@@ -0,0 +1,24 @@
+from random import randint
+import datetime
+
+#Code: D€m0
+#CODIGO DE USO LIBRE#
+dates = input("\033[1;37m[\033[1;31m#\033[1;37m]\033[1;33mGenerar fecha de cad. \033[1;37msi/no: ")
+
+if dates == "si":
+ def dategen():
+   now = datetime.datetime.now()
+   date = ""                                                                         
+   month = str(randint(1, 9))
+   current_year = str(now.year)                                                 
+   year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+   date = month + "/" + year
+
+   return date
+
+else:
+    exit()
+
+def main():
+    print(f"\033[1;36m----> \033[1;37m{dategen()}")
+main()
